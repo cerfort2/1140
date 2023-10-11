392523fb54a4f0a6bd70f04ffa1f5ac76310c11e
Krishna <Krishna6303@gmail.com>
2023-10-10 20:23:51 -0400

    Working on fake track boolean algebra

---
+++ Main_Code.py
@@ -0,0 +1,15 @@
+import sys
+from HWTrackDisplay import Ui_MainWindow
+from PyQt6 import QtWidgets
+
+#Code below to run the UI
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+
+
+
+
+sys.exit(app.exec())
