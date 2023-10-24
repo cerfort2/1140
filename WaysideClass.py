0c36845cff8da4b97e80fb9a99d50f5dfb05858e
Krishna <Krishna6303@gmail.com>
2023-10-23 22:31:57 -0400

    Wayside 1 setup

--- WaysideClass.py
+++ WaysideClass.py
@@ -7,5 +7,5 @@ class Wayside():
     def createTrack(self,switch:bool, crossroad:bool, light:bool, station:bool, right= "", left= ""): #Adding a new track to the list
         self.tracks.append(Track(switch, crossroad, light, station))
         if switch == True: #If it is a switch you can also specify the Left and Right Destination
-            self.tracks[self.tracks.__len__()].setLeftDest(left)
-            self.tracks[self.tracks.__len__()].setRightDest(right)
+            self.tracks[self.tracks.__len__()-1].setLeftDest(left)
+            self.tracks[self.tracks.__len__()-1].setRightDest(right)
