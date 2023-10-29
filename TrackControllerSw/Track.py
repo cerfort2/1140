71b7185ca5d405350069d256abef236d33c43273
zab67 <zab67@pitt.edu>
2023-10-28 22:45:50 -0400

    track quality of life

--- TrackControllerSw/Track.py
+++ TrackControllerSw/Track.py
@@ -4,41 +4,42 @@ import Block
 from Block import *
 class Track():
     waysides:Wayside = []
+    blocks:Block = []
 
     def __init__(self, ways:Wayside = []):
         self.waysides = ways
+        self.blocks:Block = []
+        #Create list of blocks from waysides
+        for i in range (len(self.waysides)):
+            for j in range (len(self.waysides[i].blocks)):
+                self.blocks.append(self.waysides[i].blocks[j])
+        #Sort list of blocks
+        for i in range (len(self.blocks)):
+            for j in range (i, len(self.blocks)):
+                if(self.blocks[i].name > self.blocks[j].name):
+                    hold = self.blocks[i]
+                    self.blocks[i] = self.blocks[j]
+                    self.blocks[j] = hold
+        hold = self.blocks[len(self.blocks) - 1]
+        self.blocks[len(self.blocks) - 1] = self.blocks[len(self.blocks) - 2]
+        self.blocks[len(self.blocks) - 2] = hold
  
     def getData(self):
         #Created lists
         data = [[],[],[]]
-        blocks:Block = []
-        #Create list of blocks from waysides
-        for i in range (len(self.waysides)):
-            for j in range (len(self.waysides[i].blocks)):
-                blocks.append(self.waysides[i].blocks[j])
-        #Sort list of blocks
-        for i in range (len(blocks)):
-            for j in range (i, len(blocks)):
-                if(blocks[i].name > blocks[j].name):
-                    hold = blocks[i]
-                    blocks[i] = blocks[j]
-                    blocks[j] = hold
-        hold = blocks[len(blocks) - 1]
-        blocks[len(blocks) - 1] = blocks[len(blocks) - 2]
-        blocks[len(blocks) - 2] = hold
         
         #Create data array
-        for i in range (len(blocks)):
-            if(blocks[i].getHasSwitch()):
-                data[0].append(blocks[i].getSwitch())
+        for i in range (len(self.blocks)):
+            if(self.blocks[i].getHasSwitch()):
+                data[0].append(self.blocks[i].getSwitch())
             else:
                 data[0].append(False)
-            if(blocks[i].getHasCrossroad()):
-                data[1].append(blocks[i].getCrossroad())
+            if(self.blocks[i].getHasCrossroad()):
+                data[1].append(self.blocks[i].getCrossroad())
             else:
                 data[1].append(False)  
-            if(blocks[i].getHasSignal()):
-                data[2].append(blocks[i].getSignal())
+            if(self.blocks[i].getHasSignal()):
+                data[2].append(self.blocks[i].getSignal())
             else:
                 data[2].append(False) 
         #Return data
@@ -47,45 +48,14 @@ class Track():
     def getOccupancy(self):
         #Created lists
         data = []
-        blocks:Block = []
-        #Create list of blocks from waysides
-        for i in range (len(self.waysides)):
-            for j in range (len(self.waysides[i].blocks)):
-                blocks.append(self.waysides[i].blocks[j])
-        #Sort list of blocks
-        for i in range (len(blocks)):
-            for j in range (i, len(blocks)):
-                if(blocks[i].name > blocks[j].name):
-                    hold = blocks[i]
-                    blocks[i] = blocks[j]
-                    blocks[j] = hold
-        hold = blocks[len(blocks) - 1]
-        blocks[len(blocks) - 1] = blocks[len(blocks) - 2]
-        blocks[len(blocks) - 2] = hold
         #Create a list of block names
-        for i in range (len(blocks)):
-            if(blocks[i].getOccupancy()):
-                data.append(blocks[i].name)
+        for i in range (len(self.blocks)):
+            if(self.blocks[i].getOccupancy()):
+                data.append(self.blocks[i].name)
         #Return data
         return data
 
     def setOccupancy(self, data:bool = []):
-        #Create list
-        blocks:Block = []
-        #Create list of blocks from waysides
-        for i in range (len(self.waysides)):
-            for j in range (len(self.waysides[i].blocks)):
-                blocks.append(self.waysides[i].blocks[j])
-        #Sort list of blocks
-        for i in range (len(blocks)):
-            for j in range (i, len(blocks)):
-                if(blocks[i].name > blocks[j].name):
-                    hold = blocks[i]
-                    blocks[i] = blocks[j]
-                    blocks[j] = hold
-        hold = blocks[len(blocks) - 1]
-        blocks[len(blocks) - 1] = blocks[len(blocks) - 2]
-        blocks[len(blocks) - 2] = hold
         #Insert Occupancy
-        for i in range (len(blocks)):
-            blocks[i].setOccupancy(data[i])
+        for i in range (len(self.blocks)):
+            self.blocks[i].setOccupancy(data[i])
