126b4c3df77c31d87af77292b886bff189fd6562
connorpaladino1 <connorjpaladino@gmail.com>
2023-10-29 16:23:57 -0400

    2 more new files

---
+++ train_model_gui/train_model.py
@@ -0,0 +1,8 @@
+
+
+
+class train_model():
+
+    def __init__(self):
+        self.current_speed = 0
+        self.current_power = 0
---
+++ train_model_gui/train_model_interface.py
@@ -0,0 +1,7 @@
+
+
+
+class train_model_interface():
+
+    def __init__(self):
+        self.trains = []
