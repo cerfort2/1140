ea501a786ceb2f85278aa1d95044d9908f7883a0
connorpaladino1 <122750312+connorpaladino1@users.noreply.github.com>
2023-11-15 15:23:34 -0500

    Update train_model_interface_software.py

--- train_model_gui/train_model_interface_software.py
+++ train_model_gui/train_model_interface_software.py
@@ -15,3 +15,17 @@ class train_model_interface_software():
     #access a single train object in the list
     def access_train(self, train_num: int) -> train_model_software:
         return self.trains[train_num - 1]
+
+    #update all trains
+    def update_trains(self) -> None:
+        for train in self.trains:
+            train.update_train()
+
+    #get occupancy list
+    def get_occupancies(self) -> list[str]:
+        out_list = []
+
+        for train in self.trains:
+            out_list.append(train.get_occupancy())
+        
+        return out_list
