aa155c1023c6656b6bb6378ff812ecf413e7451a
Connor Paladino <connorjpaladino@gmail.com>
2023-11-16 12:00:08 -0500

    updated for gravity

--- train_model_gui/GUI INTEGRATION/train_model_software.py
+++ train_model_gui/GUI INTEGRATION/train_model_software.py
@@ -221,7 +221,10 @@ class train_model_software():
     #THIS DOES NOT YET ACCOUNT FOR GRAVITY
     def calculate_acceleration(self, power: float, mass: float, delta_time: float) -> float:
         self.acceleration = math.sqrt(power / (2 * mass * delta_time))
-        return self.acceleration - 1.2 * self.controller.serviceBrake - 2.3 * self.controller.eBrake
+        if self.controller.serviceBrake or self.controller.eBrake:
+            return self.acceleration - 1.2 * self.controller.serviceBrake - 2.3 * self.controller.eBrake - (9.81 * math.sin(math.radians(self.slope)))
+        else:
+            return self.acceleration - (9.81 * math.sin(math.radians(self.slope)))
     
     #calculates time traveled over a time period
     def calculate_travel(self, speed: float, delta_time: float) -> float:
