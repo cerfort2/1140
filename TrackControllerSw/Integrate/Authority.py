bb61aeed596c056198cc3f7f72271bb00d7628cd
zab67 <zab67@pitt.edu>
2023-11-15 21:00:49 -0500

    auth

--- TrackControllerSw/Integrate/Authority.py
+++ TrackControllerSw/Integrate/Authority.py
@@ -1,25 +1,20 @@
 import Block
 from Block import *
 class Authority():
-    self.route = []
+    route = []
 
     def __init__(self, blo):
         self.blocks = blo
         # trackModelAuthority.emit(first)
 
-    
-    def newRoute(rou):
+    def newRoute(self, rou):
         #Add a new route when train created
         self.route.append(rou)
 
     def calculate(self, num):
         #For every block that is occupied count the blocks to the next stop
-        occupied = False
-        for i in range(len(self.blocks)):
-            if(self.blocks[i].getOccupancy()):
-                occupied = True
-        if(not occupied):
-            return
+        if(len(self.route[num][0]) == 0):
+            return "done"
         for i in range(len(self.blocks)):
             if(self.blocks[i].getOccupancy()):
                 if(self.route[num][0][0] == self.blocks[i].getName()):
@@ -28,40 +23,28 @@ class Authority():
                     while(not stop):
                         auth1 = auth1 + 1
                         stop = self.route[num][1][auth1]
-        #Triple redundancy
-        for i in range(len(self.blocks)):
-            if(self.blocks[i].getOccupancy()):
-                if(self.route[num][0][0] == self.blocks[i].getName()):
                     stop = self.route[num][1][0]
                     auth2 = 0
                     while(not stop):
                         auth2 = auth2 + 1
                         stop = self.route[num][1][auth2]
-        for i in range(len(self.blocks)):
-            if(self.blocks[i].getOccupancy()):
-                if(self.route[num][0][0] == self.blocks[i].getName()):
                     stop = self.route[num][1][0]
                     auth3 = 0
                     while(not stop):
                         auth3 = auth3 + 1
                         stop = self.route[num][1][auth3]
-        #If authority is zero and not at a stop return null
-        if(auth1 == 0 and not self.route[num][1][0]):
-            return
-        #Triple Redundancy
-        if(auth1 == auth2):
-            self.route[num][0].pop(0)
-            self.route[num][1].pop(0)
-            return auth1
-        if(auth2 == auth3):
-            self.route[num][0].pop(0)
-            self.route[num][1].pop(0)
-            return auth2
-        if(auth1 == auth3):
-            self.route[num][0].pop(0)
-            self.route[num][1].pop(0)
-            return aith1
-        return
+                    if(auth1 == 0 and self.route[num][1][0] and len(self.route[num][0]) != 1):
+                        self.route[num][1][0] = False
+                    else:
+                        self.route[num][0].pop(0)
+                        self.route[num][1].pop(0)
+                    if(auth1 == auth2):
+                        return auth1
+                    if(auth1 == auth3):
+                        return auth1
+                    if(auth2 == auth3):
+                        return auth2
+        return "no"
                         
 
 
