65ca426860e2654ce7f2343bf6efd2b16d4bad4e
zab67 <zab67@pitt.edu>
2023-11-26 16:42:23 -0500

    b

--- TrackControllerSw/test_unittest.py
+++ TrackControllerSw/test_unittest.py
@@ -10,13 +10,12 @@ class Test_TestIncrementDecrement(unittest.TestCase):
         testing.create(listx)
         assert (testing.getOccupancy() == [0,0])
 
-    # This test is designed to fail for demonstration purposes.
     def occupancy2(self):
         testing = Track()
         listx = [[0,0],[0,0],[0,0],["A","B"]]
         testing.create(listx)
         testing.setOccupancy([1,1])
-        assert (testing.getOccupancy() == [1,1])
+        assert (testing.getOccupancy() != [0,0])
 
 if __name__ == '__main__':
     unittest.main()
