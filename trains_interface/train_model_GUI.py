73353af61d885f404301a715fa0f954bd9f8c1b5
connorpaladino1 <connorjpaladino@gmail.com>
2023-11-30 10:21:04 -0500

    added trains interface

---
+++ trains_interface/train_model_GUI.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QComboBox, QMainWindow
+from PyQt6.QtCore import QTimer
+from PyQt6 import QtCore, QtGui, QtWidgets
+from datetime import datetime
+import math
+from trains_interface.train_model_GUI_format import *
+
+
+class train_model_GUI(Ui_Form_train_model):
+  
+    def __init__(self):
+        super().__init__()
+
+
+
+
