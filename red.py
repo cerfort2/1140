33fe0ed8ff4cd27a0474afd391a826715e625116
Zack Baldwin <zab67@pitt.edu>
2023-12-13 22:21:15 -0500

    red

--- red.py
+++ red.py
@@ -4,9 +4,9 @@ def plc(blocks:Block = []):
     loop1 = False
     loop2 = False
     leg = False
-    for i in range(0, 16):
+    for i in range(0, 27):
         loop1 = loop1 or blocks[i].getOccupancy()
-    for i in range(43, 67):
+    for i in range(43, 52):
         loop2 = loop2 or blocks[i].getOccupancy()
     for i in range(32, 38):
         leg = leg or blocks[i].getOccupancy()
@@ -18,12 +18,12 @@ def plc(blocks:Block = []):
     if(not (True) or blocks[10].getOccupancy()):
         blocks[9].setSwitch(True)
     #Signal C10
-    if(blocks[9].getOccupancy()):
+    if(blocks[9].getOccupancy() and not(blocks[10].getOccupancy() or blocks[11].getOccupancy())):
         blocks[9].setSignal(False)
     else:
         blocks[9].setSignal(True)
     #Signal D11
-    if(blocks[10].getOccupancy()):
+    if(blocks[10].getOccupancy() and not(blocks[9].getOccupancy() or blocks[8].getOccupancy())):
         blocks[10].setSignal(False)
     else:
         blocks[10].setSignal(True)
@@ -167,7 +167,7 @@ def plc(blocks:Block = []):
     else:
         blocks[52].setSignal(True)
     #Signal N66
-    if(blocks[65].getOccupancy()):
+    if(blocks[65].getOccupancy() and not(loop2)):
         blocks[65].setSignal(False)
     else:
         blocks[65].setSignal(True)
@@ -178,75 +178,75 @@ def plcc(blocks:Block = []):
     loop1 = False
     loop2 = False
     leg = False
-    for i in range(0, 16):
+    for i in range(0, 27):
         loop1 = loop1 or blocks[i].getOccupancy()
-    for i in range(43, 67):
+    for i in range(43, 52):
         loop2 = loop2 or blocks[i].getOccupancy()
     for i in range(32, 38):
         leg = leg or blocks[i].getOccupancy()
 
     #Signal C10
-    if(blocks[9].getOccupancy() and (blocks[10].getOccupancy() or blocks[11].getOccupancy())):
+    if((blocks[8].getOccupancy() or blocks[9].getOccupancy()) and (blocks[10].getOccupancy() or blocks[11].getOccupancy())):
         stoppage.append(blocks[9].getName())
     #Signal D11
-    if(blocks[10].getOccupancy() and (blocks[9].getOccupancy() or blocks[8].getOccupancy())):
+    if((blocks[10].getOccupancy() or blocks[11].getOccupancy()) and (blocks[9].getOccupancy() or blocks[8].getOccupancy())):
         stoppage.append(blocks[10].getName())
     #Signal Yard
     if(blocks[76].getOccupancy() and loop1):
         stoppage.append(blocks[76].getName())
     #Signal A1
-    if(blocks[0].getOccupancy() and (blocks[15].getOccupancy() or blocks[16].getOccupancy())):
+    if((blocks[0].getOccupancy() or blocks[1].getOccupancy()) and (blocks[15].getOccupancy() or blocks[16].getOccupancy())):
         stoppage.append(blocks[0].getName())
     #Signal E15
-    if(blocks[14].getOccupancy() and (blocks[15].getOccupancy() or blocks[16].getOccupancy())):
+    if((blocks[13].getOccupancy() or blocks[14].getOccupancy()) and (blocks[15].getOccupancy() or blocks[16].getOccupancy())):
         stoppage.append(blocks[14].getName())
     #Signal F16
-    if(blocks[15].getOccupancy() and (blocks[0].getOccupancy() or blocks[1].getOccupancy())):
+    if((blocks[15].getOccupancy() or blocks[16].getOccupancy()) and (blocks[0].getOccupancy() or blocks[1].getOccupancy())):
         stoppage.append(blocks[15].getName())
     #Signal H27
-    if(blocks[26].getOccupancy() and (blocks[27].getOccupancy() or blocks[28].getOccupancy())):
+    if((blocks[25].getOccupancy() or blocks[26].getOccupancy()) and (blocks[27].getOccupancy() or blocks[28].getOccupancy())):
         stoppage.append(blocks[26].getName())
     #Signal H28
-    if(blocks[27].getOccupancy() and (loop1 or blocks[76].getOccupancy())):
+    if((blocks[27].getOccupancy() or blocks[28].getOccupancy()) and (loop1 or blocks[76].getOccupancy())):
         stoppage.append(blocks[27].getName())
     #Signal T76
-    if(blocks[75].getOccupancy() and (loop1 or blocks[76].getOccupancy())):
+    if((blocks[74].getOccupancy() or blocks[75].getOccupancy()) and (loop1 or blocks[76].getOccupancy())):
         stoppage.append(blocks[76].getName())
     #Signal H32
-    if(blocks[31].getOccupancy() and (leg or blocks[70].getOccupancy())):
+    if((blocks[30].getOccupancy() or blocks[31].getOccupancy()) and (leg or blocks[70].getOccupancy())):
         stoppage.append(blocks[31].getName())
     #Signal H33
-    if(blocks[32].getOccupancy() and (blocks[71].getOccupancy() or blocks[72].getOccupancy())):
+    if((blocks[32].getOccupancy() or blocks[33].getOccupancy()) and (blocks[71].getOccupancy() or blocks[72].getOccupancy())):
         stoppage.append(blocks[32].getName())
     #Signal R72
-    if(blocks[71].getOccupancy() and (blocks[32].getOccupancy() or blocks[33].getOccupancy())):
+    if((blocks[71].getOccupancy() or blocks[72].getOccupancy()) and (blocks[32].getOccupancy() or blocks[33].getOccupancy())):
         stoppage.append(blocks[71].getName())
     #Signal H38
-    if(blocks[37].getOccupancy() and (blocks[38].getOccupancy() or blocks[39].getOccupancy())):
+    if((blocks[36].getOccupancy() or blocks[37].getOccupancy()) and (blocks[38].getOccupancy() or blocks[39].getOccupancy())):
         stoppage.append(blocks[37].getName())
     #Signal H39
-    if(blocks[38].getOccupancy() and (blocks[37].getOccupancy() or blocks[36].getOccupancy())):
+    if(blocks[38].getOccupancy() or blocks[39].getOccupancy()) and (blocks[37].getOccupancy() or blocks[36].getOccupancy()):
         stoppage.append(blocks[38].getName())
     #Signal Q71
-    if(blocks[70].getOccupancy() and (leg)):
+    if((blocks[69].getOccupancy() or blocks[70].getOccupancy()) and (leg)):
         stoppage.append(blocks[70].getName())
     #Signal H43
-    if(blocks[42].getOccupancy() and (loop2)):
+    if((blocks[41].getOccupancy() or blocks[42].getOccupancy()) and (loop2)):
         stoppage.append(blocks[42].getName())
     #Signal H44
-    if(blocks[43].getOccupancy() and (blocks[66].getOccupancy() or blocks[67].getOccupancy())):
+    if((blocks[43].getOccupancy() or blocks[44].getOccupancy()) and (blocks[66].getOccupancy() or blocks[67].getOccupancy())):
         stoppage.append(blocks[43].getName())
     #Signal O67
-    if(blocks[66].getOccupancy() and (loop2)):
+    if((blocks[66].getOccupancy() or blocks[67].getOccupancy()) and (loop2)):
         stoppage.append(blocks[66].getName())
     #Signal J52
-    if(blocks[51].getOccupancy() and (blocks[52].getOccupancy() or blocks[53].getOccupancy())):
+    if((blocks[50].getOccupancy() or blocks[51].getOccupancy()) and (blocks[52].getOccupancy() or blocks[53].getOccupancy())):
         stoppage.append(blocks[51].getName())
     #Signal J53
-    if(blocks[52].getOccupancy() and (blocks[51].getOccupancy() or blocks[50].getOccupancy())):
+    if((blocks[52].getOccupancy() or blocks[53].getOccupancy()) and (loop2)):
         stoppage.append(blocks[52].getName())
     #Signal N66
-    if(blocks[65].getOccupancy() and (blocks[51].getOccupancy() or blocks[50].getOccupancy())):
+    if((blocks[64].getOccupancy() or blocks[65].getOccupancy()) and (loop2)):
         stoppage.append(blocks[65].getName())
 
     return stoppage
